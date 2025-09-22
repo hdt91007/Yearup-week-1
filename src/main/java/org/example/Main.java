f984f6b7139d13f1aae7515fc356b3c2f3eb5199
Hdt91007 <hdt91007@gmail.com>
2025-09-22 14:26:51 -0700

    My commit is like a save point in a game. I can always go back to this point

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("programing is wow wee zow weee");
+        System.out.println("my Name is Hoang Tran");
+    }
+}
